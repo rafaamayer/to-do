7d08345d9764ece3e5c4261ec3c4ec6cbcf98fb5
Rafael Mayer <rafa.m.mayer@hotmail.com>
2021-10-31 16:29:15 -0300

    feature: implement date-time

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ import { criarBotao, concluirTarefa, deletarTarefa } from "./components/botao.js
 const criarTarefa = (evento) => {
     evento.preventDefault();
 
-    const input = document.querySelector("[data-form-input");
-
+    const input = document.querySelector("[data-form-input]");
+    const data = document.querySelector("[data-form-date]");
+    
     const valor = input.value;
+    const valorData = data.value;
     
     const tarefa = document.createElement("li");
     tarefa.classList.add("task");
 
-    const conteudo = `<p class="content">${valor}</p>`;
-    
+    const conteudo = `<p class="content">${valor} ${valorData}</p>`;
+
     tarefa.innerHTML = conteudo
+
     tarefa.appendChild(criarBotao("Concluir", "check-button", concluirTarefa));
     tarefa.appendChild(criarBotao("Deletar", null, deletarTarefa));
 
