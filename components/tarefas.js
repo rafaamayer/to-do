5de2976046f0ed76ffb189f30e650526d3391e2c
Rafael Mayer <rafa.m.mayer@hotmail.com>
2021-11-01 17:59:45 -0300

    feature: refactor components

---
+++ components/tarefas.js
@@ -0,0 +1,46 @@
+import { criarBotao, concluirTarefa, deletarTarefa } from "./botao.js";
+
+const handleValores = () => {
+
+    const input = document.querySelector("[data-form-input]");
+    const data = document.querySelector("[data-form-date]");
+
+    const valor = input.value;
+    const valorData = moment(data.value).format('DD/MM/YYYY HH:mm');
+
+    const informacoes = {
+        'valor': valor,
+        'data': valorData
+    }
+
+    return informacoes
+}
+
+const armazenarValores = () => {
+    localStorage.setItem("tarefas", JSON.stringify(handleValores()))
+}
+
+const criarTarefa = (evento) => {    
+    evento.preventDefault();
+
+    const novosValores = handleValores()
+    const {valor, data} = novosValores
+
+    const tarefa = document.createElement("li");
+    tarefa.classList.add("task");
+    
+    const conteudo = `<p class="content">${data} ° ${valor}</p>`;
+
+    tarefa.innerHTML = conteudo
+
+    tarefa.appendChild(criarBotao("Concluir", "check-button", concluirTarefa));
+    tarefa.appendChild(criarBotao("Deletar", null, deletarTarefa));
+
+    const lista = document.querySelector("[data-list]");
+    
+    armazenarValores()
+
+    lista.appendChild(tarefa);
+}
+
+export { criarTarefa, handleValores }
